5aeefdf939f6b2610e79ca577221721bf93dd3f4
yama <devthinkrec@gmail.com>
2020-12-08 20:17:33 +0900

    [1-1][update] sample.jsxに記述（手打ち）

---
+++ src/entries/sample.jsx
@@ -0,0 +1,11 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootEl = document.getElementById('root');
+
+ReactDOM.render(
+    <h1>Helllo react.</h1>,
+    rootEl,
+);
