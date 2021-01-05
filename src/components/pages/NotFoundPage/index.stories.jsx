bd8dd6b1e8ec7b67418079c6bd2692cdff1635ae
yama <devthinkrec@gmail.com>
2021-01-05 20:26:51 +0900

    [2-1][update] NotFoundPage：storyを作成してStorybookに表示

---
+++ src/components/pages/NotFoundPage/index.stories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import NotFoundPage from ".";
+
+export default { title: 'pages/NotFoundPage' };
+
+export const notFoundPage = () => <NotFoundPage />;
+notFoundPage.story = { name: 'ノットファウンド' };
