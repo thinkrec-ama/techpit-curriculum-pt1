bdfea4e289a77d61a30973f7dffaf442b0d564d3
yama <devthinkrec@gmail.com>
2021-01-05 20:08:54 +0900

    [2-1][update] storybook設定：Storybookのstory表示時に実行する処理記述

---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { addDecorator } from '@storybook/react';
+
+addDecorator(storyFn => (
+    <MemoryRouter
+        initialEntries={['/', 'posts']}
+    >
+        {storyFn()}
+    </MemoryRouter>
+));
